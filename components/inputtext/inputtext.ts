377fc58d8768e21ebe10061b331e41138352cdc2
adam.andrzejewski@jitsolutions.pl <adam.andrzejewski@jitsolutions.pl>
2016-09-14 13:17:29 +0200

    initial commit

---
+++ components/inputtext/inputtext.ts
@@ -0,0 +1,54 @@
+import {NgModule,Directive,ElementRef,HostListener,Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Directive({
+    selector: '[pInputText]',
+    host: {
+        '[class.ui-inputtext]': 'true',
+        '[class.ui-corner-all]': 'true',
+        '[class.ui-state-default]': 'true',
+        '[class.ui-widget]': 'true',
+        '[class.ui-state-hover]': 'hover',
+        '[class.ui-state-focus]': 'focus',
+        '[class.ui-state-disabled]': 'isDisabled()'
+    }
+})
+export class InputText {
+
+    hover: boolean;
+    
+    focus: boolean;
+    
+    constructor(protected el: ElementRef) {}
+    
+    @HostListener('mouseover', ['$event']) 
+    onMouseover(e) {
+        this.hover = true;
+    }
+    
+    @HostListener('mouseout', ['$event']) 
+    onMouseout(e) {
+        this.hover = false;
+    }
+    
+    @HostListener('focus', ['$event']) 
+    onFocus(e) {
+        this.focus = true;
+    }
+    
+    @HostListener('blur', ['$event']) 
+    onBlur(e) {
+        this.focus = false;
+    }
+    
+    isDisabled() {
+        return this.el.nativeElement.disabled;
+    }
+}
+
+@NgModule({
+    imports: [CommonModule],
+    exports: [InputText],
+    declarations: [InputText]
+})
+export class InputTextModule { }
